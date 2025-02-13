ff57b05f6b9e9b8ea25de1e35ed8a21bf40818c2
helloMinh <phandinhminh48@gmail.com>
2025-02-13 23:11:41 +0700

    m

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import session from 'express-session';
 import router from './routes/index.routes.js';
 import morgan from 'morgan';
 import Database from './configs/database.js';
@@ -10,19 +9,10 @@ import Database from './configs/database.js';
 const app = express();
 
 // Middleware
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
-}))
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    rolling: true,
-    cookie: {
-        maxAge: Number(process.env.SESSION_LIFETIME)
-    }
 }));
 app.use(cors());
 
