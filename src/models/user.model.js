70276d722a4600a2b00df34599628238f34b7776
helloMinh <phandinhminh48@gmail.com>
2025-02-17 14:57:34 +0700

    fixed user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -24,64 +24,18 @@ var userSchema = new mongoose.Schema({
         id: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'PROJECT'
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String
-        },
-        num_participants: {
-            type: Number,
-            default: 1
-        },
-        daydone: {
-            type: Date,
-            default: Date.now
         }
     }],
     task: [{
         id: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'TASK'
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String
-        },
-        num_participants: {
-            type: Number,
-            default: 0
-        },
-        daydone: {
-            type: Date,
-            default: Date.now
         }
     }],
     friends: [{
         id: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'USER'
-        },
-        avatar: {
-            type: String
-        },
-        name: {
-            type: String,
-            required: true,
-            index: true
-        },
-        status: {
-            type: String,
-            enum: ['active', 'inactive'],
-            default: 'active',
-        },
-        description: {
-            type: String
         }
     }],
     phone: {
